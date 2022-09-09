c930d747154b6c23895952f975fc2e0df9fc7347
jbairoso <jbairoso@gmail.com>
2022-09-09 09:12:37 -0700

    transaction store

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,7 +1,18 @@
 let db;
-const request = indexedDB.open('budget-tracker', 1);
+const request = indexedDB.open("budget-tracker", 1);
 
 request.onupgradeneeded = function (event) {
-    const db = event.target.result;
-    db.createObjectStore("nw_transaction", { autoIncrement: true });
+  const db = event.target.result;
+  db.createObjectStore("nw_transaction", { autoIncrement: true });
 };
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+
+window.addEventListener('online', checkDatabase);
