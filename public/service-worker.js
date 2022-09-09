333112e4cb09cb6c3a8c0ed759aff8c7248d7df2
jbairoso <jbairoso@gmail.com>
2022-09-09 09:30:08 -0700

    cache-ing

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,19 +1,30 @@
-const APP_PREFIX = 'BudgetTracker-';
-const VERSION = 'version_01';
+const APP_PREFIX = "BudgetTracker-";
+const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-    "/",
-    "./icons/icon-72x72.png",
-    "./icons/icon-96x96.png",
-    "./icons/icon-128x128.png",
-    "./icons/icon-144x144.png",
-    "./icons/icon-152x152.png",
-    "./icons/icon-192x192.png",
-    "./icons/icon-384x384.png",
-    "./icons/icon-512x512.png",
-    "./index.html",
-    "./css/styles.css",
-    "./js/db.js",
-    "./js/index.js"
+  "/",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png",
+  "./index.html",
+  "./css/styles.css",
+  "./js/db.js",
+  "./js/index.js",
 ];
+
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Cache-ING");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
